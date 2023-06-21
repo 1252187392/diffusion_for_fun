8f8edc37153effc70fcbab9baae5eb19e5ae00b1
郑昕 <1252187392@qq.com>
2023-06-21 15:25:52 +0800

    update

--- app/web_demo.py
+++ app/web_demo.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 import sys
 import os
 
@@ -6,17 +8,13 @@ import torch
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 import gradio as gr
-import cv2
 import importlib
 
-from diffusers import DiffusionPipeline, DDIMScheduler,DDPMScheduler,StableDiffusionLatentUpscalePipeline
-from models.train_step import generate_image,generate_SR_image
+from diffusers import DiffusionPipeline, DDIMScheduler, DDPMScheduler, StableDiffusionLatentUpscalePipeline
+from models.train_step import generate_image, generate_SR_image
 
 
 from lora_diffusion import (
-    inject_trainable_lora,
-    save_lora_weight,
-    extract_lora_ups_down,
     monkeypatch_lora,
     tune_lora_scale,
 )
@@ -39,74 +37,51 @@ pipeline = DiffusionPipeline.from_pretrained(
         torch_dtype=torch.float16,
         safety_checker=None
 )
-from xformers.ops import MemoryEfficientAttentionFlashAttentionOp
-pipeline.enable_xformers_memory_efficient_attention(MemoryEfficientAttentionFlashAttentionOp)
-#pipeline.unet.enable_xformers_memory_efficient_attention()
-pipeline = pipeline.to("cuda",torch_dtype=torch.float16)
+
+#from xformers.ops import MemoryEfficientAttentionFlashAttentionOp
+#pipeline.enable_xformers_memory_efficient_attention(MemoryEfficientAttentionFlashAttentionOp)
+pipeline = pipeline.to("cuda", torch_dtype=torch.float16)
 
 if conf.load_weights:
     pipeline.unet.load_attn_procs(conf.load_weights)
-
-monkeypatch_lora(pipeline.text_encoder, torch.load(os.path.join(conf.load_weights, "lora_text_encoder.pt")),
-                 target_replace_module=["CLIPAttention"])
-
-model_id = "stabilityai/sd-x2-latent-upscaler"
-#model_id = "stabilityai/stable-diffusion-x4-upscaler"
-upscaler = StableDiffusionLatentUpscalePipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-upscaler.to("cuda")
+    monkeypatch_lora(pipeline.text_encoder, torch.load(os.path.join(conf.load_weights, "lora_text_encoder.pt")),
+                     target_replace_module=["CLIPAttention"])
 
 
 def text_to_image(height, width, prompt, prompt2, negative_prompt, seed, text_lora_scale=1, diffusion_lora_scale=1, steps=25):
-    #prompt = f'masterpiece,{prompt}' if 'masterpiece' not in prompt else prompt
     print('used prompt:', prompt)
     steps = int(steps)
     tune_lora_scale(pipeline.text_encoder, text_lora_scale)
     with torch.no_grad():
-        images1 = generate_SR_image(pipeline, prompt, steps, seed, 2, 'cuda',
-                             None, negative_prompt, diffusion_lora_scale, (width,height),upscaler)
-        images2 = generate_SR_image(pipeline, prompt2, steps,seed,2,'cuda',
-                             None,negative_prompt,diffusion_lora_scale, (width,height),upscaler)
+        images1 = generate_image(pipeline, prompt, steps, seed, 2, 'cuda',
+                                 None, negative_prompt, diffusion_lora_scale, (width, height))
+        images2 = generate_image(pipeline, prompt2, steps, seed, 2, 'cuda',
+                                 None, negative_prompt, diffusion_lora_scale, (width, height))
 
     return images1+images2
 
 
-eg1 = """
-masterpiece,1girl, alternate costume, ass, bangs, very long hair, white hair, white shirt, white skirt, window, bare arms, blue e
-yes, blunt bangs, blush, bow, breasts, chair, chromatic aberration, clothes pull, crop top, easy chair, eric (tianqijiang), flower knot, from
-below, from side, genshin impact, groin, hair bow, hair ribbon, indoors, kamisato ayaka, long hair, looking at viewer, medium breasts, midriff
-, miniskirt, on chair, one knee, panties, parted lips, pink bow, pink panties, pleated skirt, ponytail, red ribbon, revision, ribbon, shirt, s
-ide-tie panties, sideboob, sidelighting, signature, skirt, skirt pull, sleeveless, sleeveless shirt, solo, thighs, tress ribbon, underwear
-"""
-eg2 = """
-masterpiece,1girl, animal ears, bangs, breasts, cherry blossoms, cowboy shot, crystalfly (genshin impact), detached sleeves, earrings, falling petals, floating hair, floppy ears, floral print, flower knot, fox ears, genshin impact, hair between eyes, hair ornament, hand up, japanese clothes, jewelry, kusunokinawate, long hair, long sleeves, looking at viewer, medium breasts, nontraditional miko, petals, purple eyes, red skirt, ribbon trim, shirt, sidelocks, skirt, sleeveless, sleeveless shirt, solo, tassel, thighs, turtleneck, white sleeves, wide sleeves, yae miko
-"""
-eg3 = """
-masterpiece,1girl, animal ears, bangs, bare shoulders, blush, breasts, earrings, fantongjun, fox ears, genshin impact, hair ornament, jewelry, large breasts, long hair, looking at viewer, naked towel, open mouth, pink hair, purple eyes, raised eyebrows, sidelocks, solo, thighs, towel, very long hair, yae miko
-"""
-eg4 = """
-modelshoot style,(best quality, masterpiece:1.1), (realistic:1.4),intricate elegant, (highly detailed),sharp focus, dramatic,photorealistic,A beautiful Chinese girl,<lora:nana_v10:1.2:FACEH>,(High Detail), smile,china dress,full body,high-heeled shoes,In the lobby,Screen window, corridor,Curtains of fluttering yarn,((Mottled light and shadow,warm light ,depth of field)),
-"""
-eg5 = """
-(8k, RAW photo, best quality, masterpiece:1.2), (realistic, photo-realistic:1.37),1girl,cute, naked,standing,cityscape, night, rain, wet, professional lighting, photon mapping, radiosity, physically-based rendering
-"""
+def init_examples():
+    prompts = [line.strip() for line in open('datas/prompts.txt', encoding='utf8') if len(line.strip()) > 0]
+    neg_prompts = [line.strip() for line in open('datas/neg_prompts.txt', encoding='utf8') if len(line.strip()) > 0]
+    seed = 137
+    scale = 1
+    steps = 25
+    size = 512
+    examples = []
+    for idx, prompt in enumerate(prompts):
+        examples.append([size, size, prompt, '', neg_prompts[idx%len(neg_prompts)], seed, scale, scale, steps])
+    return examples
 
-neg_eg1 = 'worst quality, low quality, medium quality, deleted, lowres, comic, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, jpeg artifacts, signature, watermark, username, blurry'
 
-neg_eg2 = '(makeup:1.8),(smooth skin:1.3), anime, illustration, 3d, sepia, painting, cartoons, sketch, (worst quality:2), (low quality:2), (normal quality:2), lowres, bad anatomy, bad hands, normal quality, ((monochrome)), ((grayscale:1.2)), futanari, full-package_futanari, penis_from_girl, newhalf, collapsed eyeshadow, multiple eyebrows, vaginas in breasts, pink hair, holes on breasts, fleckles, stretched nipples, gigantic penis, nipples on buttocks, analog, analogphoto, anal sex, signatre, logo, render,'
-examples = [[512, 512, eg1.strip(), '', neg_eg1, 137, 1, 1, 25],
-            [512, 512,eg2.strip(), '', neg_eg1, 137, 1, 1, 25],
-            [512, 512,eg3.strip(), '', neg_eg1, 137, 1, 1, 25],
-            [512, 512,eg4.strip(), '', neg_eg1, 137, 1, 1, 25],
-            [512, 512,eg5.strip(), '', neg_eg1, 137, 1, 1, 25]
-            ]
-# interface = gr.Interface(fn=to_black, inputs="image", outputs="image")
-# interface.launch(share=True)
 interface = gr.Interface(fn=text_to_image,
-                         inputs=[gr.Slider(256, 1024, value=512, step=64, label="height", info="height"),
-                                gr.Slider(256, 1024, value=512, step=64, label="width", info="width"),
-                                    "text", "text", "text", gr.Slider(1, 20000, value=1, label="seed", info="随机种子"),
-                                    gr.Slider(0, 1, value=1, label="text_lora_scale", info="text_lora_scale"),
-                                    gr.Slider(0, 1, value=1, label="diffusion_lora_scale", info="diffusion_lora_scale"),
-                                    gr.Slider(1, 50, value=1, step=1, label="steps", info="steps")],
-                         outputs=["image"] * 4, examples=examples)
+                         inputs=[
+                             gr.Slider(256, 1024, value=512, step=64, label="height", info="height"),
+                             gr.Slider(256, 1024, value=512, step=64, label="width", info="width"),
+                             "text", "text", "text", gr.Slider(1, 20000, value=1, label="seed", info="随机种子"),
+                             gr.Slider(0, 1, value=1, label="text_lora_scale", info="text_lora_scale"),
+                             gr.Slider(0, 1, value=1, label="diffusion_lora_scale", info="diffusion_lora_scale"),
+                             gr.Slider(1, 50, value=1, step=1, label="steps", info="steps")],
+                         outputs=["image"] * 4, examples=init_examples())
+
 interface.launch(share=False)
